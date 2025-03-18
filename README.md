331706ad1ef2e27989ecb8ba7d9306867b85e934
Joanna <joanna.tarnowska1@gmail.com>
2025-03-18 20:03:29 +0100

    Added test setup and improved README file

--- README.md
+++ README.md
@@ -1,31 +1,36 @@
 # Setup of the project
-1. Create an empty folder on your computer where the project will be placed.
-2. Open Terminal/Command Prompt and clone the repository to this new folder:
-`git clone https://github.com/jtarnowska/flyability-qa-automation-test.git`
-3. Open project folder in IDE.
-4. Open Terminal, move to the cloned project folder and create virtual environment: 
+
+1. Clone the repository with `git clone`.
+2. Move to the cloned project folder and create virtual environment: 
+
 `python -m venv .venv`
-5. Activate the virtual environment:
+
+3. Activate the virtual environment:
 - for windows: `.venv/Scripts/Activate`
 - for mac: `source .venv/bin/activate`
-6. Install dependencies:
-`pip install -r requirements.txt
+4. Install dependencies:
+
+`pip install -r requirements.txt`
+
+5. Run tests.
 
 # How to run tests
-By default all tests are run on Chrome browser but they are also working on Firefox and Edge (by using proper parameter)
+By default, all tests are run on Chrome browser, but they are also working on Firefox and Edge (by using an option `--browser_name`)
+
 ## All tests at once
-`pytest .\tests\map_test.py` 
-## Specific test
-`pytest .\tests\map_test.py::{name_of_the_test}`
-Example: `pytest .\tests\map_test.py::test_distance_display`
+`pytest tests`
+
 ## Test with generating HTML report
-`pytest .\tests\map_test.py --html=./reports/report.html --self-contained-html`
+`pytest tests --html=./reports/report.html --self-contained-html`
+
 ## Run tests on specific browser
-`pytest .\tests\map_test.py::test_hover_layers --browser_name chrome`
-`pytest .\tests\map_test.py::test_hover_layers --browser_name firefox`
-`pytest .\tests\map_test.py::test_hover_layers --browser_name edge`
+`pytest tests --browser_name chrome`
+
+`pytest tests --browser_name firefox`
+
+`pytest tests --browser_name edge`
 
 # Additional information:
-- screenshots are available in .screenshots folder after running tests
-- reports are available from the link visible in the terminal after running tests and are saved in .reports directory
-- browsers are opened in en_GB language to make sure all selectors are always correct
+- screenshots are available in `screenshots` folder after running tests
+- reports are available after running tests and are saved in `reports` directory
+- browsers are opened in `en-GB` language to make sure all selectors are always correct
--- tests/map_test.py
+++ tests/map_test.py
@@ -1,36 +1,39 @@
 import re
-
+import pytest
 from pages.map_page import MapPage
 
-def test_distance_display(driver):
+@pytest.fixture(scope="class")
+def map_page(driver):
     map_page = MapPage(driver)
     map_page.open_map()
     map_page.accept_cookies()
-    map_page.click_on_directions_button()
-    map_page.enter_starting_point("Lausanne")
-    map_page.enter_destination_point("Genève")
-    map_page.press_enter()
+    return map_page
 
-    driving_text = map_page.get_text_over_driving_icon()
-    assert "Driving" in driving_text, f"Expected 'Driving' but got '{driving_text}'"
+class TestMapPage:
+    def test_distance_display(self, map_page):
+        map_page.open_map()
+        map_page.click_on_directions_button()
+        map_page.enter_starting_point("Lausanne")
+        map_page.enter_destination_point("Genève")
+        map_page.press_enter()
 
-    map_page.click_on_driving_button()
+        driving_text = map_page.get_text_over_driving_icon()
+        assert "Driving" in driving_text, f"Expected 'Driving' but got '{driving_text}'"
 
-    distance_display = map_page.get_distance_display_text()
-    pattern = r"^\d+(\.\d+)?\s*km$"
-    assert re.match(pattern, distance_display), f"Distance format is incorrect: {distance_display}"
+        map_page.click_on_driving_button()
 
-    is_scrolling_successful = map_page.scroll_to_restaurants_icon()
-    assert is_scrolling_successful, "Scrolling to the restaurants button failed"
+        distance_display = map_page.get_distance_display_text()
+        pattern = r"^\d+(\.\d+)?\s*km$"
+        assert re.match(pattern, distance_display), f"Distance format is incorrect: {distance_display}"
 
-    map_page.save_screenshot_for_specific_test("test_distance_display")
+        is_scrolling_successful = map_page.scroll_to_restaurants_icon()
+        assert is_scrolling_successful, "Scrolling to the restaurants button failed"
 
-def test_hover_layers(driver):
-    map_page = MapPage(driver)
-    map_page.open_map()
-    map_page.accept_cookies()
+        map_page.save_screenshot_for_specific_test("test_distance_display")
 
-    is_tooltip_visible = map_page.get_tooltip_over_layers_button()
-    assert is_tooltip_visible, "Tooltip over layers button is not visible after hovering"
+    def test_hover_layers(self, map_page):
+        map_page.open_map()
+        is_tooltip_visible = map_page.get_tooltip_over_layers_button()
+        assert is_tooltip_visible, "Tooltip over layers button is not visible after hovering"
 
-    map_page.save_screenshot_for_specific_test("test_hover_layers")
+        map_page.save_screenshot_for_specific_test("test_hover_layers")
